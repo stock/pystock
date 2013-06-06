3f53f744ab2a5499cae1e32a97ad485d11f1bcb0
jack <stockss@gmail.com>
2013-06-06 15:23:05 +0800

    modify output format

--- stock.py
+++ stock.py
@@ -1,4 +1,4 @@
-﻿#! /usr/bin/python
+﻿#! /bin/python
 # coding="utf-8"
 
 import urllib
@@ -6,13 +6,12 @@ f=urllib.urlopen("http://hq.stock.sohu.com/viewMyStockListq?cn_000028,cn_600609"
 buf=f.read()
 f.close()
 stocklist=eval(buf[buf.index(',[')+1:buf.index('])')])
-print u"股票代码 股票名称 当前价 涨跌幅 涨跌额 总手 现手 成交金额 换手率 最高价 最低价 市盈率 昨收盘 今开盘"
+##         0        1       2       3     4     5    6      7       8       9     10     11     12      13   
+##int u"股票代码 股票名称 当前价 涨跌幅 涨跌额 总手 现手 成交金额 换手率 最高价 最低价 市盈率 昨收盘 今开盘"
+print u"\n股票代码\t股票名称\t当前价\t涨跌幅\t换手率"
+print  "------------------------------------------------------"
 for stock in stocklist:
-    for stockitem in stock:
-        print stockitem,
-    print
+    print stock[0],'\t',stock[1].decode('gbk').encode('utf8'),'\t',stock[2],'\t',stock[3],'\t',stock[8]
+
+print
     
-print stocklist[0][2]+chr(9),13.05,chr(9),float(stocklist[0][2])/13.05
-print stocklist[1][2]+chr(9),3.94,chr(9),float(stocklist[1][2])/3.94
-print 600,'==>',int(600*float(stocklist[0][2])/float(stocklist[1][2]))
-print 2000,'==>',int(2000*float(stocklist[1][2])/float(stocklist[0][2]))
